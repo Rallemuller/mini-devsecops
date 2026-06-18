a91b76fbcaa7734947ac73274527d67f0d1f676b
Rallemuller <madsen992@hotmail.com>
2026-06-18 22:53:26 +0200

    Tilføjer Semgrep SAST scanning

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Velkommen til min DevSecOps app!');
+// FEJL: Brugerinput bliver sat direkte ind i en kommando uden validering
+app.get('/search', (req, res) => {
+  const brugerInput = req.query.q;
+  const { execSync } = require('child_process');
+  const resultat = execSync('grep -r ' + brugerInput + ' /data');
+  res.send(resultat.toString());
 });
 
 app.listen(3000, () => console.log('App kører på port 3000'));
